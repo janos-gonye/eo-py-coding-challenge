0942c0b2549394d08e4263bcfe71ce52f7f69c95
János Gönye <janos.gonye@gmail.com>
2026-03-24 23:29:55 +0100

    Enable CORS

---
+++ public_api/src/public_api/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants module for the Public API application.
+"""
+
+CORS_ALLOW_ORIGINS = ["http://localhost:5174"]
+CORS_ALLOW_METHODS = ["*"]
+CORS_ALLOW_HEADERS = ["*"]
--- public_api/src/public_api/main.py
+++ public_api/src/public_api/main.py
@@ -3,12 +3,21 @@ Main module for the Public API application.
 """
 
 from fastapi import FastAPI
-from public_api.routers.ip import router as ip_router
+from fastapi.middleware.cors import CORSMiddleware
 
+from public_api import constants
+from public_api.routers.ip import router as ip_router
 from public_api.app_logging import setup_logging
 
 setup_logging()
 
 app = FastAPI(title="Public API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=constants.CORS_ALLOW_ORIGINS,
+    allow_methods=constants.CORS_ALLOW_METHODS,
+    allow_headers=constants.CORS_ALLOW_HEADERS,
+)
 
 app.include_router(ip_router)
